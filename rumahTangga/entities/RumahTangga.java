f17b017aad6ba2d2224856d456ad63e749677048
Meidy Kusdianto <2381060@unai.edu>
2024-12-02 20:05:07 +0700

    Mengerjakan bagian entities/RumahTangga.java (Meidy Kusdianto)

---
+++ rumahTangga/entities/RumahTangga.java
@@ -0,0 +1,25 @@
+package rumahTangga.entities;
+
+public class RumahTangga {
+    private String todo;
+    private Integer id;
+
+    public RumahTangga() {
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(final String todo) {
+        this.todo = todo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+}
